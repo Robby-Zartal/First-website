b827444abd3aa60756130610bf10fd68da763a4c
robby-zartal <robert.holloway49@gmail.com>
2024-11-15 07:15:25 +0100

    reverted changes

---
+++ upload.js
@@ -0,0 +1,16 @@
+document.getElementById('upload-form').addEventListener('submit', function(event){
+    event.preventDefault();
+    const formData = new FormData(this);
+    fetch('upload.php', {
+        method: 'POST',
+        body: formData
+    })
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('message').innerText = data;
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        document.getElementById('message').innerText = 'An error occured during the upload.';
+    });
+});
